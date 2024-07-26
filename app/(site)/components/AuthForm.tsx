a15c6a15c8d605c2d7e7ea7ac0a392e89adff1f6
Nguyen Sang <89461242+sorrynodoor32@users.noreply.github.com>
2024-07-26 21:38:56 +0700

    #3-sidebar-chat

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -3,8 +3,8 @@
 import axios from 'axios'
 import toast from "react-hot-toast"
 
-import { signIn } from "next-auth/react"
-import { useCallback, useState } from "react"
+import { signIn, useSession } from "next-auth/react"
+import { useCallback, useEffect, useState } from "react"
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form"
 
 import AuthSocialButton from "./AuthSocialButton"
@@ -12,13 +12,22 @@ import Input from "@/app/components/inputs/Input"
 import Button from "@/app/components/Button"
 
 import { BsGithub, BsGoogle } from "react-icons/bs"
+import { useRouter } from 'next/navigation'
 
 type Variant = 'LOGIN' | 'REGISTER'
 
 const AuthForm = () => {
 
+      const session = useSession()
+      const router = useRouter()
       const [variant, setVariant] = useState<Variant>('LOGIN')
       const [isLoading, setIsLoading] = useState(false)
+
+      useEffect(() => {
+            if(session?.status === 'authenticated'){
+                  router.push("/users")
+            }
+      },[session?.status, router])
 
       const toggleVariant = useCallback(() => {
             if(variant === 'LOGIN'){
@@ -44,7 +53,7 @@ const AuthForm = () => {
             if(variant === 'REGISTER'){
                   //Axios Register
                   axios.post("/api/register", data)
-                  .then(() => toast.success('User have been created!'))
+                  .then(() => signIn('credentials', data))
                   .catch(() => toast.error('Something went wrong'))
                   .finally(() => {
                         setIsLoading(false)
@@ -64,6 +73,7 @@ const AuthForm = () => {
                         }
                         if(callback?.ok && !callback?.error){
                               toast.success('Logged in!')
+                              router.push("/users")
                         }
                   })
                   .finally(()=> {
